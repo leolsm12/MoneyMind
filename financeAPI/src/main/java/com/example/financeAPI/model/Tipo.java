87dfbff52ef9cd4a2fdf5e1abaf140ea93813938
Leonardo <leoschaffer@hotmail.com>
2025-02-18 14:51:06 -0300

    model Tipo

--- financeAPI/src/main/java/com/example/financeAPI/model/Tipo.java
+++ financeAPI/src/main/java/com/example/financeAPI/model/Tipo.java
@@ -1,4 +1,18 @@
 package com.example.financeAPI.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Tipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "descricao", nullable = false)
+    private String descricao; // Descrição do tipo (exemplo: Alimentação, Lazer, Salário, etc.)
+
+    @Column(name = "categoria", nullable = false)
+    private String categoria; // Categoria: 'Gasto' ou 'Receita'
 }
